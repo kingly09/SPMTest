7ba4fd6975a1071c4234d9eb90eb944d3ef5a707
kingly <libintm@163.com>
2019-01-11 16:43:48 +0800

    1.0.0

---
+++ Sources/SPMTest/SPMTest.swift
@@ -0,0 +1,17 @@
+struct SPMTest {
+    var text = "Hello, World!"
+}
+
+public class SPMPublicTest {
+  public var text = "SPMPublicTest"
+  public init(text:String) {
+    self.text = text
+  }
+}
+
+public struct SPMPublicStructTest {
+  public var text = "SPMPublicStructTest"
+  public init(text:String) {
+    self.text = text
+  }
+}
